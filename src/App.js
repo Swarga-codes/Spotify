f1c5d4b327492f4ffe0fec1411df9f879bdc2090
Swarga-codes <sd3526@srmist.edu.in>
2023-01-14 18:52:44 +0530

    added reducer and initial state

--- src/App.js
+++ src/App.js
@@ -3,22 +3,30 @@ import './App.css';
 import Login from './components/Login/Login';
 import { getTokenFromUrl } from './spotify-config';
 import SpotifyWebApi from 'spotify-web-api-js';
-const spotify=SpotifyWebApi();
+import Player from './components/Player/Player';
+import { useDataLayerValue } from './Datalayer';
+const spotify= new SpotifyWebApi();
 function App() {
+
   const[token,setToken]=useState();
+  const[{},dispatch]=useDataLayerValue();
   useEffect(()=>{
     const hash=getTokenFromUrl();
     window.location.hash="";
     const _token=hash.access_token;
     if(_token){
       setToken(_token);
+      spotify.setAccessToken(_token);
+      spotify.getMe().then((user) => {
+        console.log("The user is: ",user)
+      });
     }
     console.log("token",token);
-  })
+  },[]);
   return (
     <div className="App">
     {token?
-    <h1>Sucessfully Logged in</h1>
+  <Player/>
   :
   <Login/>
   }
---
+++ src/components/Player/Player.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Player() {
+  return (
+    <div className='player'>
+    <h1>Welcome to spotify Player</h1>
+    </div>
+  )
+}
+
+export default Player;
