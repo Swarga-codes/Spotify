ec51dd51a04b5efa8ac320d3ae0de33cfb7870c9
Swarga-codes <sd3526@srmist.edu.in>
2023-01-16 04:35:41 +0530

    done with header part

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
-
+import './Header.css'
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import { Avatar } from '@mui/material';
+import { useDataLayerValue } from '../../Datalayer';
 function Header() {
+    const[{user},dispatch]=useDataLayerValue();
   return (
     <div className='header'>
     <div className="header_search">
-    
+   <SearchOutlinedIcon className='header_search_icon'/>
+   <input type="text" placeholder='What do you want to listen to?'/>
     </div>
     <div className="header_avatar">
-    
+   <Avatar alt="no preview" src={user?.images[0]?.url} />
+   <h1>{user?.display_name}</h1>
     </div>
     </div>
   )
--- src/components/MainBody/Body.jsx
+++ src/components/MainBody/Body.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Header from '../Header/Header'
 import './Body.css'
+import { useDataLayerValue } from '../../Datalayer'
 function Body({spotify}) {
+    const[{discover_weekly},dispatch]=useDataLayerValue();
   return (
     <div className='body'>
-    <Header/>
+    <Header spotify={spotify}/>
+    <div className="body_details">
+    <img src={discover_weekly?.images[0]?.url} alt="no preview" />
+    <div className="body_title">
+    <strong>PLAYLIST</strong>
+    <h1>Discover Weekly</h1>
+    <p>{discover_weekly?.description}</p>
+    </div>
+    </div>
     </div>
   )
 }
